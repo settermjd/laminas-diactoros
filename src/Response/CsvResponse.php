e22ee24f1ff2b676d3072f81650ccc60e89a01a1
Matthew Setter <matthew.setter@gmail.com>
2020-02-21 22:06:03 +0100

    Update the class docblock headers for the laminas-diactoros project

--- src/Response/CsvResponse.php
+++ src/Response/CsvResponse.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-diactoros for the canonical source repository
- * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-diactoros/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/laminas/laminas-diactoros for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-diactoros/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-diactoros/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
--- src/Response/DownloadResponseTrait.php
+++ src/Response/DownloadResponseTrait.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @see       https://github.com/laminas/laminas-diactoros for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-diactoros/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-diactoros/blob/master/LICENSE.md New BSD License
+ */
 
 
 namespace Zend\Diactoros\Response;
